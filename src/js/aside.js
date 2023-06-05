c1eb7d6fccee0e8ead7468c3b2310dcb47ae2423
andrsweb <proflinervk@gmail.com>
2023-06-05 10:05:28 +0700

    add body scroll lock for asides

--- src/js/aside.js
+++ src/js/aside.js
@@ -1,23 +1,31 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
+import { setTargetElement, getTargetElement } from './common/global'
+
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
 
-	showAndCloseAside()
+	showAndCloseAside('#aside_lock', '#aside_lock')
+	showAndCloseAside('#asideImages_lock', '#asideImages_lock')
 })
 
-const showAndCloseAside = () => {
+const showAndCloseAside = (selector, lock) => {
 	const configButton = document.querySelectorAll('.configure_text')
-	const aside = document.querySelector('.aside')
+	const aside = document.querySelector(selector)
 	const asideClose = document.querySelector('.aside__close')
+	setTargetElement(document.querySelector(lock))
 
-	if (!configButton || !aside || !asideClose) return
+	if (!configButton || !aside || !asideClose ) return
 
 	configButton.forEach(button => {
 		button.addEventListener('click', () => {
+			setTargetElement(document.querySelector(lock))
 			aside.classList.add('showed')
+			disableBodyScroll(getTargetElement(), { reserveScrollBarGap: true })
 		})
 	})
 
 	asideClose.addEventListener('click', () => {
 		aside.classList.remove('showed')
+		enableBodyScroll(getTargetElement())
 	})
 }
