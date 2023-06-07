81ac8583855f255412a23101a68603ef7f125d1b
andrsweb <proflinervk@gmail.com>
2023-06-07 08:55:07 +0700

    add hide show inputs

--- src/js/signin.js
+++ src/js/signin.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
 	'use strict'
+
+	hideInputValue()
 })
+
+const hideInputValue = () => {
+	const buttons = document.querySelectorAll('.eye__wrapper')
+
+	if( ! buttons.length) return
+
+	buttons.forEach( button => {
+		button.addEventListener( 'click', () => {
+			const input = button.closest('span').querySelector('input')
+			if( !button.classList.contains( 'clicked')) {
+				button.classList.add( 'clicked')
+				input.type = 'text'
+			} else {
+				button.classList.remove( 'clicked')
+				input.type = 'password'
+			}
+		})
+	})
+}
