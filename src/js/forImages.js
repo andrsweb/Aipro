9a05dfba643f656391449cd107a373b0e21f7d07
andrsweb <proflinervk@gmail.com>
2023-06-14 19:39:10 +0700

    changed js

--- src/js/forImages.js
+++ src/js/forImages.js
@@ -9,20 +9,18 @@ const addSelected = (wrapper, selector) => {
 
 	const fieldWrapper = document.querySelectorAll(wrapper)
 
-	if( !fieldWrapper.length) return
+	if (!fieldWrapper.length) return
 
-	fieldWrapper.forEach( item => {
-		item.addEventListener('click', e => {
-			const target = e.target
-			const image = target.closest(selector)
-	
-			if (!image.classList.contains('selected')) {
-				image.classList.add('selected')
-	
-			} else {
-				image.classList.remove('selected')
-			}
-		})
+	document.querySelector('body').addEventListener('click', e => {
+		const target = e.target
+		const image = target.closest(selector)
+
+		if (!image.classList.contains('selected')) {
+			image.classList.add('selected')
+
+		} else {
+			image.classList.remove('selected')
+		}
 	})
 }
 
